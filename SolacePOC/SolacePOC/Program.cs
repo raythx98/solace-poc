a33085c477bc73ce0fe81dd94e7e1adb21a2e153
Ray Toh <raythx98@gmail.com>
2023-01-06 17:45:17 +0800

    Add Random Publish

---
+++ SolacePOC/SolacePOC/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using static MessagingClient.Publisher;
+
+// using SolacePOC;
+namespace SolacePOC{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+            string[] countries = { "singapore", "australia" };
+            for (int i = 0; i < 100; i++)
+            {
+                string message = rdn.Next(100000, 1000000).ToString();
+                string country = countries[rdn.Next(2)%2];
+                string carplate = String.Format("{0}{1}{2}{3}",
+                    Convert.ToChar('A'+rdn.Next(25)), 
+                    Convert.ToChar('A'+rdn.Next(25)), 
+                    rdn.Next(1000,10000), 
+                    Convert.ToChar('A'+rdn.Next(25)));
+                string userid = rdn.Next(100000, 1000000).ToString();
+                string topic = String.Format("getgo/{0}/booking/{1}/{2}",
+                    country, carplate, userid);
+                // Console.WriteLine(topic + message);
+                Publish(topic, message);
+            }
+            
+        }
+    }
+
+}
---
+++ run_solace_software.sh
@@ -0,0 +1,1 @@
+docker run -d -p 8080:8080 -p 55554:55555 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2222:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
